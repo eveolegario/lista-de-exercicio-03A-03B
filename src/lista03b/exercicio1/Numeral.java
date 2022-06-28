72be544dc2a09a9f8a8823fc887235300604570d
eveolegario <evelynolegario93@gmail.com>
2022-06-28 18:36:58 -0300

    criação do exercício 1 "Numeral"

---
+++ src/lista03b/exercicio1/Numeral.java
@@ -0,0 +1,17 @@
+package lista03b.exercicio1;
+
+public class Numeral {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+        int contador = 1;
+        while (contador <= 100) {
+            System.out.println(contador);
+            contador++;
+        }
+
+    }
+}

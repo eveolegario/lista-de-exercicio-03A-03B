d263feb89c00043bb558837d82d03a5f47f55ac5
eveolegario <evelynolegario93@gmail.com>
2022-06-28 18:39:03 -0300

    criação do exercício 5 "Incrementador numeral"

---
+++ src/lista03b/exercicio5/IncrementadorNumeral.java
@@ -0,0 +1,11 @@
+package lista03b.exercicio5;
+
+public class IncrementadorNumeral {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100000; i += 1000) {
+            System.out.println(i);
+        }
+    }
+}
